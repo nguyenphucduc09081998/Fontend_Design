dcda38cabc40a55657a580c5e48fa39879624b10
DESKTOP-OR0SSCH\PhucDuc <phucducktpm@gmail.com>
2019-12-10 09:30:13 +0700

    filter

---
+++ js/filter-detail.js
@@ -0,0 +1,74 @@
+$(document).ready(function(){
+	
+	$('#price-range-submit').hide();
+
+	$("#min_price,#max_price").on('change', function () {
+
+	  $('#price-range-submit').show();
+
+	  var min_price_range = parseInt($("#min_price").val());
+
+	  var max_price_range = parseInt($("#max_price").val());
+
+	  if (min_price_range > max_price_range) {
+		$('#max_price').val(min_price_range);
+	  }
+
+	  $("#slider-range").slider({
+		values: [min_price_range, max_price_range]
+	  });
+	  
+	});
+
+
+	$("#min_price,#max_price").on("paste keyup", function () {                                        
+
+	  $('#price-range-submit').show();
+
+	  var min_price_range = parseInt($("#min_price").val());
+
+	  var max_price_range = parseInt($("#max_price").val());
+	  
+	  if(min_price_range == max_price_range){
+
+			max_price_range = min_price_range + 100000;
+			
+			$("#min_price").val(min_price_range);		
+			$("#max_price").val(max_price_range);
+	  }
+
+	  $("#slider-range").slider({
+		values: [min_price_range, max_price_range]
+	  });
+
+	});
+
+
+	$(function () {
+	  $("#slider-range").slider({
+		range: true,
+		orientation: "horizontal",
+		min: 0,
+		max: 5000000,
+		values: [500000, 2500000],
+		step: 100,
+
+		slide: function (event, ui) {
+		  if (ui.values[0] == ui.values[1]) {
+			  return false;
+		  }
+		  
+		  $("#min_price").val(ui.values[0].toLocaleString('en-US')+'đ');
+		  $("#max_price").val(ui.values[1].toLocaleString('en-US')+'đ');
+		}
+	  });
+
+	  $("#min_price").val($("#slider-range").slider("values", 0).toLocaleString('en-US')+'đ');
+	  $("#max_price").val($("#slider-range").slider("values", 1).toLocaleString('en-US')+'đ');
+
+	});
+
+
+	
+
+});
